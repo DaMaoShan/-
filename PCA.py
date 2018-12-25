aab6c5a58f9777863f288ff056636a2c26da598e
DaMaoShan <xingygqy@163.com>
2018-12-25 12:22:43 +0800

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -26,33 +26,60 @@ Y = []
 #     X.append(x)
 #     Y.append(y)
 
-NUM = 100
-for i in range(NUM):   # other
-    x = rand.uniform(-10, 10)
-    y = rand.uniform(-10, 10)
-    X.append(x)
-    Y.append(y)
+# NUM = 100
+# for i in range(NUM):   # other
+#     x = rand.uniform(-10, 10)
+#     y = rand.uniform(-10, 10)
+#     X.append(x)
+#     Y.append(y)
 
-X1 = []
-Y1 = []
-for i in range(NUM):
-    Y1.append(Y[i])
-    X1.append(Y1[i]*(1-(Y[i])/10))
-    # X1.append(Y[i])
+# X1 = []
+# Y1 = []
+# for i in range(NUM):
+#     Y1.append(Y[i])
+#     X1.append(Y1[i]*(1-(Y[i])/10))
+#     # X1.append(Y[i])
 
-D = np.array([X1, Y1])
-D = D.T
-pca = PCA()
+# D = np.array([X1, Y1])
+# D = D.T
+# pca = PCA()
 
-pca.fit(D)
-new_axis = pca.components_
+# pca.fit(D)
+# new_axis = pca.components_
 
-print(new_axis)
+# print(new_axis)
 
 # print(pca.explained_variance_)  # 特征值
 # print(pca.explained_variance_ratio_)  # 特征值标准化
 
 # [0.9924... 0.0075...]
+
+NUM = 1000
+for i in range(NUM):   # ellipse 
+    x = rand.uniform(-10, 10)
+    y = rand.uniform(-10, 10)
+    d1 = np.sqrt((x-6.5)**2 + (y-6.5)**2)
+    d2 = np.sqrt((x+6.5)**2 + (y+6.5)**2)
+    if d1 + d2 <= 20:
+        X.append(x)
+        Y.append(y) 
+
+m1 = np.mean(X)
+m2 = np.mean(Y)
+M = np.array([m1, m2]).reshape(2, 1)       
+D = np.array([X, Y])
+D = D - M
+D = D.T
+
+pca = PCA()
+pca.fit(D)
+new_axis = pca.components_
+
+print('新座标轴向量,即特征向量：\n', new_axis[0, :], '\n', new_axis[1, :])
+print('对应的特征值:\n', pca.explained_variance_)  
+print('协方差矩阵：\n', np.dot(D.T, D), '\n')
+D1 = np.dot(new_axis, D.T)  # 新坐标系中的坐标(数据)
+print('协方差矩阵：\n', np.dot(D1, D1.T), '\n')
 
 axes = plt.gca()
 axes.set_xlim([-5, 5])
